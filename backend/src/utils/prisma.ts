ece8276d93f85330928b0404c81e923647952846
muchemi wamuyu <muchemiedwin68@gmail.com>
2026-03-26 14:54:40 +0300

    prisma database error fixes

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -1,8 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL, // FORCING THE RUNTIME ENV
+      },
+    },
+    // Useful for debugging in Docker
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
